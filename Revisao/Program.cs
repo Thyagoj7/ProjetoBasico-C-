f69d36468a376766db26e71ffa518b40c9edc8a8
Thyago <t.y_josef7@hotmail.com>
2021-01-14 13:44:37 -0300

    implementando a primeira opcao

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -1,4 +1,12 @@
-﻿using Internal;
+﻿using System.Runtime.Serialization;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Collections.Concurrent;
+using System.Reflection.Emit;
+using System.Data;
+using System.Reflection.Metadata.Ecma335;
+using System.Reflection;
+using System.Globalization;
 using System;
 
 namespace Revisao
@@ -7,6 +15,10 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+
+            Aluno[] alunos = new Aluno[5]; //Um array aluno ele precisa ser iniciado: Iniciamos com 5
+            var indiceAluno = 0;  //Vai mostrar o indice do array onde sera colocado o aluno adicionado
+
             string opcaoUsuario = ObterOpcaoUsuario(); //O methodo sera chamado por aqui
 
     
@@ -18,9 +30,29 @@ namespace Revisao
                 {
                     case "1":
                             //TODO: adicionar aluno
+                            Console.WriteLine ("Informar o nome do aluno:");
+                            Aluno aluno = new Aluno(); //Instanciando um objeto do tipo aluno
+                            aluno.Nome = Console.ReadLine(); //Ja vai por direto do console o valor :vai setar o valor no Objeto
+
+                            Console.WriteLine("Informar a nota do aluno:");
+                            //var nota = decimal.Parse(Console.ReadLine()); //O codigo poderia ficar sem o var nota só assim (decimal.Parse(Console.ReadLine());) Mas se tentar por um valor sem ser decimal iria dar um erro no codigo 
+                            //O if vai analisar se o valor passado é realmente um decimal
+                           if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                           {
+                               aluno.Nota=nota;
+                           } 
+                           else
+                           {
+                               throw new ArgumentException("Valor da nota deve ser decimal");
+                           }
+
+                           alunos[indiceAluno] = aluno; //vai direcionar o aluno para o indice do array
+                           indiceAluno++;   //apos a alocação do aluno damos um ++ para o procimo indice ser o proximo numero
+                        
                         break;
                     case "2":
                             //TODO: Lista de Alunos
+
                         break;
                     case "3":
                             //TODO: Calcular média geral
